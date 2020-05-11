02eec8641f52a43320f09f9979b29e6793720164
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2020-05-11 12:27:04 +0200

    Update pom.xml and release.sh to release to maven central

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -z "$1" ];
 then echo "usage: release.sh <releaseVersion>"
-else mvn release:prepare release:perform -DreleaseVersion=$1 -Dtag=v$1 -Darguments=-DaltDeploymentRepository=maven.intendia.com::default::http://maven.intendia.com/libs-release-local
+else mvn release:prepare release:perform -DreleaseVersion=$1 -Dtag=v$1
 fi
